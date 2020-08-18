210f91699f43c78dc42155f25b9986906aef4d95
hector moreno <hector3.mt@gmail.com>
2020-08-18 16:18:21 -0700

    adding maven project

---
+++ test/src/main/java/jar/App.java
@@ -0,0 +1,23 @@
+package jar;
+import org.joda.time.LocalTime;
+
+/**
+ * Hello world!
+ *
+ *
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	LocalTime currentTime = new LocalTime();
+    	System.out.println("The current local time is: " + currentTime);
+    	
+        System.out.println( "Hello World!!!!!!!!!!!!!!" );
+    }
+}
+
+/*
+ *
+ * */
